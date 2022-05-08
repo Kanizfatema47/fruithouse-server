55a0d774eac485730297c4bc4e9777a1feea6009
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-05-08 14:49:09 +0600

    port changing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port = 5000;
+const port = 8000;
 const app = express();
 
 require("dotenv").config();
@@ -18,38 +18,48 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //   const collection = client.db("items").collection("item");
 //   console.log('database connecteed sucessfully');
 //   // perform actions on the collection object
-  
+
 //   client.close();
 // });
 
-
-async function run(){
-    try{
-      await client.connect();
-      const productcollection = client.db("items").collection("item");
-
-      //Featching data
-
-      app.get("/products", async (req, res) => {
-        const query = {};
-        const cursor = productcollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
-      });
-
-    }
-    finally{
-
-    }
-  }
-  run().catch(console.dir);
-
-
-
-
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("Server is running succesfully. Now connect mongodb to your server")
 })
+async function run() {
+    try {
+        await client.connect();
+        const productcollection = client.db("items").collection("item");
+
+        //Featching data
+
+        app.get("/products", async (req, res) => {
+            const query = {};
+            const cursor = productcollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+        // for single data
+
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productcollection.findOne(query);
+            res.send(product);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("server is running");
